d8962d0fe8523a3b391872962ed5d317f9cf4314
Dex.Wasy <dexwasy@gmail.com>
2018-03-10 11:32:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,56 @@
+
+package com.example.android.justjava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    int quantity = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void increment(View view) {
+        quantity = quantity + 1;
+        displayquantity(quantity);
+    }
+
+    public void decrement(View view) {
+        quantity = quantity - 1;
+        displayquantity(quantity);
+
+    }
+//
+    public void submitOrder(View view) {
+        int price = quantity * 5;
+        String priceMessage = "Total $" + price;
+        priceMessage = priceMessage + " \n Thank You";
+        displayMessage(priceMessage);
+    }
+
+    private void displayquantity(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+
+}
